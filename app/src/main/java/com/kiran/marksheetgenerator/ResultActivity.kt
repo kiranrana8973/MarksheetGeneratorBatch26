458e53e40c6d271436d90a008b6274872af5a004
Kiran Rana <kiranrana8973@gmail.com>
2021-06-22 10:32:36 +0545

    received arraylist

--- app/src/main/java/com/kiran/marksheetgenerator/ResultActivity.kt
+++ app/src/main/java/com/kiran/marksheetgenerator/ResultActivity.kt
@@ -2,6 +2,8 @@ package com.kiran.marksheetgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kiran.marksheetgenerator.model.Student
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,17 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
 
         //Receiving array list
+
+        val listStudent = intent.getStringArrayListExtra("listStudent") as ArrayList<Student>
+        var totalAndroidMarks = 0
+        var totalApiMarks = 0
+        for (student in listStudent) {
+            totalAndroidMarks += student.android!!.toInt()
+            totalApiMarks += student.api!!.toInt()
+        }
+
+        Toast.makeText(this, "Android : $totalAndroidMarks", Toast.LENGTH_SHORT).show()
+
 
 
     }
